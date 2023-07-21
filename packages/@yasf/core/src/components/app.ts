fc9b947cc7ac9fb323e399bbb19d14807bdd5c85
Mikael Giacomini <hello@mikaelgiacomini.com>
2023-07-21 16:26:14 +0100

    fix(app): use generic app construct instead of cdktf one

--- packages/@yasf/core/src/components/app.ts
+++ packages/@yasf/core/src/components/app.ts
@@ -1,3 +1,4 @@
+import { IConstruct } from "constructs";
 import { getResolverComponents } from "../cloud-providers";
 
 export interface GenericAppOptions {
@@ -6,9 +7,11 @@ export interface GenericAppOptions {
     };
 }
 
-export class GenericApp {
-    constructor(options: GenericAppOptions) {
-        const providerKey = options.context['provider']
-        return new (getResolverComponents(providerKey)).app(options)
-    }
+export interface IGenericApp extends IConstruct {
+    synth(): void;
 }
+
+export const GenericApp = function (options: GenericAppOptions) {
+    const providerKey = options.context['provider']
+    return new (getResolverComponents(providerKey)).app(options)
+} as unknown as new (options: GenericAppOptions) => IGenericApp
